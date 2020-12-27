6248f2f45cdb52ae1160d79b78a1f90f47a0c7c5
dinhngochieu21 <dinhngochieu21@gmail.com>
2020-12-27 22:19:50 +0700

    search

--- js/test.js
+++ js/test.js
@@ -11,23 +11,21 @@ $(document).ready(function(){
 
   $(".dropdown-item").on("click",function(){
     
-  
-    $(".active").on("click",function(){
-      $(this).removeClass("active");
-      console.log("a");
-      return;
-    })
 
-    $(this).addClass("active");
+    $(this).addClass("active").siblings().removeClass("active");
     x = $(".active").text();
     return
 
     
-  
+  })
 
-    
-   
-    
+  $(".item a").on("click",function(){
+    var value = $(this).text().toLowerCase();
+    alert(value)
+    $(".room_post").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      
+    });
   })
 
  
@@ -36,7 +34,7 @@ $(document).ready(function(){
 
   $("#search-btn").on("click",function(){
     x+=" "  + $("#myInput").val().toLowerCase();
-    alert(x);
+    
     $(".room_post").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(x) > -1)
       
